1a9636167fbfbd5b470119b06900b16974bb49b5
Mustafa Yapar <97470314+Mustafayapar@users.noreply.github.com>
2025-08-19 11:29:33 +0300

    Buildings bloks

---
+++ Contracts/Products/ProductCreatedEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Products
+{
+    public sealed record ProductCreatedEvent(
+    Guid Id,
+    string Name,
+    decimal Price,
+    int Stock,
+    DateTime CreatedAtUtc
+);
+}
---
+++ Contracts/Products/ProductUpdatedEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Products
+{
+    public sealed record ProductUpdatedEvent(
+    Guid Id,
+    string Name,
+    decimal Price,
+    int Stock,
+    DateTime UpdatedAtUtc
+);
+}
